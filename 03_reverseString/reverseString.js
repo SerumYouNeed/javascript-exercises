a22796fcc8e74704b47d01e2ec3a1500cd39ee12
Greg Miler <greg.miler@gmail.com>
2025-03-26 13:40:26 +0100

    03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let listString = string.split("");
+    let reverse = [];
+    for (let i = listString.length; i > 0; i--) {
+        reverse.push(listString.pop());
+    }
+    console.log(reverse);
+    let newString = reverse.join("")
+    return newString
 };
 
 // Do not edit below this line
