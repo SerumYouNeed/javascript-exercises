07e342d8570ef889ea153160dfddd8655cc87932
Greg Miler <greg.miler@gmail.com>
2025-03-27 13:16:17 +0100

    04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    const newArr = [];
+    for ( const x of arr ) {
+
+        if ( theArgs.includes(x) ) {
+            continue;
+        } else {
+            newArr.push(x);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
