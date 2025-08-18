cacdb07ab3c9c4f7c1a3404ed837584a050fb54a
Greg Miler <greg.miler@gmail.com>
2025-08-18 23:54:04 +0200

    shoping list

--- dom/js-file.js
+++ dom/js-file.js
@@ -7,6 +7,20 @@ btn.addEventListener("click", beClicked)
 function beClicked() {
     const val = inpt.value;
     inpt.value = "";
+
+    const isidelistItem = document.createElement("li");
+    const inisdeBtn = document.createElement("button");
+    const insideSpan = document.createElement("span");
     
+    inisdeBtn.textContent = "Delete"
+    insideSpan.textContent = val
+    isidelistItem.appendChild(insideSpan);
+    isidelistItem.appendChild(inisdeBtn);
+
+    lst.appendChild(isidelistItem);
+
+    inisdeBtn.addEventListener("click", () => lst.removeChild(isidelistItem));
+
+    inpt.focus();
 }
 btn.style.background = "red";
