8b720f94bca2352e165018c5f2307482ca716b58
Greg Miler <greg.miler@gmail.com>
2025-08-19 23:32:17 +0200

    shoping list

--- dom/js-file.js
+++ dom/js-file.js
@@ -24,3 +24,36 @@ function beClicked() {
     inpt.focus();
 }
 btn.style.background = "red";
+
+// EXAMINE THE DOCUMENT OBJECT
+console.dir(document);
+console.dir(document.domain);
+console.dir(document.URL);
+
+// SELECTORS
+let header = document.getElementById("header")
+header.textContent = "Hello"; // to zmienia sam text, nie zwraca uwagi na styl
+header.innerText = "hej"; // zmienia tekst ale uważa na styl
+header.innerHTML = "<h3>jo!</h3>" // to osadza cały tag h3 w header;
+let listIt = document.getElementsByClassName("li-elements"); // elements, to zwraca nodelist więc jak chcę coś zrobić dla całości to iteruj jak dla jednostki to indeksy
+let alllistIt = document.getElementsByTagName("li-elements"); // elements, to zwraca nodelist więc jak chcę coś zrobić dla całości to iteruj jak dla jednostki to indeksy
+
+// PARENTS
+let parent1 = header.parentNode; // po kropce można dojść do głównego rodzica
+let parent2 = header.parentElement; // właściwie można tego używać zamiennie
+
+// CHILDREN
+let chold1 = parent1.childNodes; // to zwraca nodelist, uwaga na text node - to są np. break line pomiędzy elementami które znajdą się w liście
+let chold2 = parent2.children; // to zwraca HTML collection więc bez tych textów
+chold1.firstElementChild // te metody zwrócą pierwszy element html
+chold1.lastElementChild
+
+// SIBLING
+console.log(chold1.nextElementSibling);
+console.log(chold1.previousElementSibling);
+
+// CREATE
+let newDiv = document.createElement("div");
+let txt = document.createTextNode("Hello world");
+newDiv.appendChild(txt)
+
